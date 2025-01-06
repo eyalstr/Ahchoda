613229a792ca157541a2ea30c7a769024b082dcb
eyal strauss <eyalst@justice.gov.il>
2025-01-06 16:39:59 +0200

    get CaseDisplayed input from user

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ if __name__ == "__main__":
                 exit()
 
             # Log the case ID that was found
-            log_and_print(f"######-- Case ID=({case_id}) --######", "info")
+            log_and_print(f"######-- מספר תיק:({case_id}) --######", "info", is_hebrew=True)
         except ValueError:
             log_and_print("Invalid input. Please enter a valid Case Displayed ID.", "error")
             exit()
